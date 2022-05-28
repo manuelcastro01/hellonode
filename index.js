9ae274913a5d043c4f4e776bcb8c9f5018bdf77d
manuelcastro01 <97099373+manuelcastro01@users.noreply.github.com>
2022-05-27 21:13:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+const { Pool } = require('pg');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node siu99!\n '
-  res.end(msg);
-});
+const config = {
+    host: 'db-postgresql-ams3-12882-do-user-11373447-0.b.db.ondigitalocean.com',
+    user: 'doadmin',
+    password: 'AVNS_epkzl0bfqf-3-4Z',
+    database: 'defaultdb',
+    port: 25060,
+};
+
+const pool = new Pool(config);
+
+const getBooks = async () => {
+    try {
+        const res = await pool.query('select * from books');
+        // console.log(res)
+        console.log(res.rows);
+        pool.end();
+    } catch (e) {
+        console.log(e);
+    }
+};
 
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+
+editUser();
