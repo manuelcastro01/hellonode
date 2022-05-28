5e5e715594e2a5c2aa202b5826d84347cd4b7400
manuelcastro01 <97099373+manuelcastro01@users.noreply.github.com>
2022-05-27 20:46:01 -0500

    Update retrieveData.js

--- retrieveData.js
+++ retrieveData.js
@@ -2,7 +2,7 @@ const { pool } = require("./db");
 
 async function retrieveData() {
   try {
-    const res = await pool.query("SELECT * FROM shark");
+    const res = await pool.query("SELECT * FROM Empleado");
     console.log(res.rows);
   } catch (error) {
     console.error(error);
