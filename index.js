746fc541f2c8901e16553be84d134497a1fc539a
manuelcastro01 <97099373+manuelcastro01@users.noreply.github.com>
2022-05-27 20:55:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node siu!\n'
+  const msg = 'Hello Node siu!\n '
   res.end(msg);
 });
+
+const { pool } = require("./db");
+
+async function retrieveData() {
+  try {
+    const res = await pool.query("SELECT * FROM shark");
+    console.log(res.rows);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+retrieveData()
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
